1522c2c94d7a3432ec680ae181d432a6416e359b
Bal <sandhu@thoughtworks.com>
2019-03-29 12:38:07 +1100

    Test app deployment

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Bonjour tout le monde\n")
+    self.assertEqual(hello(), "Hello tout le monde\n")
 
 if __name__ == '__main__':
   unittest.main()
