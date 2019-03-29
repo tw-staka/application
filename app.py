ef16cd4d6066ce2e7d5443fdb23eecb8853949c8
Bal <sandhu@thoughtworks.com>
2019-03-29 13:41:55 +1100

    Test demo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask('hello-world-gitops')
 
 @app.route('/')
 def hello():
-  return "Hello tout le monde\n"
+  return "Hello tout le monde, Version: 2\n"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Hello tout le monde\n")
+    self.assertEqual(hello(), "Hello tout le monde, Version: 2\n")
 
 if __name__ == '__main__':
   unittest.main()
